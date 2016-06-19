9fede7015f472cfb2e5e598c677dfe706a2861d5
Katia Prigon <katiapr89@gmail.com>
2016-06-20 01:10:35 +0200

    Create StudentsListModels.cs

---
+++ Project/MVC/Models/StudentsListModels.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YotzimLilmod.Entities;
+
+namespace YotzimLilmod.Models
+{
+    public class StudentsListModels
+    {
+        public List<Student> StudentsList { get; set; }
+    }
+}
