64ebbb329942d5189513d195d6d23b3068686f51
Katia Prigon <katiapr89@gmail.com>
2016-06-21 11:06:51 +0200

    Update ReportsController.cs

--- Project/Controllers/ReportsController.cs
+++ Project/Controllers/ReportsController.cs
@@ -34,21 +34,76 @@ namespace YotzimLilmod.Controllers
             return View();
         }
         //Add cshtml page for successing
-        public ActionResult AddHours(Hours r)
-        {
-            
-            if(ModelState.IsValid)
-            {
-                var start = Convert.ToDateTime(r.StartTime);
-                var end = Convert.ToDateTime(r.EndTime);
-                var total = end - start;
-                HoursDAL.SetHoursToTeacher(Convert.ToInt32(Session["UserID"]),r.StudentName,r.StudentLastName,r.StartTime,r.EndTime,r.Note);
-            }
-            return View("Index");
-        }
+        //public ActionResult AddHours(Hours r)
+        //{
+
+        //    if(ModelState.IsValid)
+        //    {
+        //        var start = Convert.ToDateTime(r.StartTime);
+        //        var end = Convert.ToDateTime(r.EndTime);
+        //        var total = end - start;
+        //        HoursDAL.SetHoursToTeacher(Convert.ToInt32(Session["UserID"]),r.StudentName,r.StudentLastName,r.StartTime,r.EndTime,r.Note);
+        //    }
+        //    return View("Index");
+        //}
         public ActionResult AllTeachersData()
         {
             return View();
+        }
+        public ActionResult AddHours()
+        {
+            return View();
+        }
+        public ActionResult UpdateHours(Hours h)
+        {
+            //var user = TeacherDAL.GetTeacherByUserID(Convert.ToInt32(Session["UserID"]));
+            string startTime = h.InputDate + " " + h.InputStartHours;
+            DateTime start = new DateTime();
+            start = DateTime.ParseExact(startTime, "yyyy-MM-dd HH:mm", null);
+
+            string endTime = h.InputDate + " " + h.InputEndHours;
+            DateTime end = new DateTime();
+            end = DateTime.ParseExact(endTime, "yyyy-MM-dd HH:mm", null);
+
+
+            TimeSpan temp = end.Subtract(start);
+            decimal totalHours = temp.Hours;//= Convert.ToDecimal();
+            decimal totalMinutes = temp.Minutes;
+            string totalString;
+            if (totalHours != 0)
+            {
+                totalString = Convert.ToString(totalHours) + "." + Convert.ToString(totalMinutes - totalHours * 60);
+            }
+            else totalString = "0." + Convert.ToString(totalMinutes);
+
+            bool isEmptyRow = TeacherDAL.UpdateTeacherHours(Convert.ToInt32(Session["UserID"]), start, end, Convert.ToDecimal(totalString), h.Note);
+            //TeacherDAL.UpdateTeacherReport()
+            var list = TeacherDAL.GetHoursByTeacher(Convert.ToInt32(Session["UserID"]));
+            HoursListModels m = new HoursListModels
+            {
+                TeacherHoursList = list
+            };
+            return View("ShowTeacherHoursList", m);
+        }
+        public ActionResult ShowTeacherHoursList()
+        {
+            var list = TeacherDAL.GetHoursByTeacher(Convert.ToInt32(Session["UserID"]));
+            HoursListModels m = new HoursListModels
+            {
+                TeacherHoursList = list
+            };
+            return View("ShowTeacherHoursList", m);
+        }
+        public ActionResult DeleteHours(int? id)
+        {
+            TeacherDAL.RemoveHouresByID(id);
+
+            var list = TeacherDAL.GetHoursByTeacher(Convert.ToInt32(Session["UserID"]));
+            HoursListModels m = new HoursListModels
+            {
+                TeacherHoursList = list
+            };
+            return View("ShowTeacherHoursList", m);
         }
     }
 }
