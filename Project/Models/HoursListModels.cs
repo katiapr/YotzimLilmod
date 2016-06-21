b27a2197b4373c16b0b0f6389234213aead73ad2
Katia Prigon <katiapr89@gmail.com>
2016-06-21 11:12:37 +0200

    Create HoursListModels.cs

---
+++ Project/Models/HoursListModels.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YotzimLilmod.Entities;
+
+namespace YotzimLilmod.Models
+{
+    public class HoursListModels
+    {
+       public List<Hours> TeacherHoursList{get;set;}
+    }
+}
