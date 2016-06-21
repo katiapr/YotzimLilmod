c023bbc241cdb36bf322ecc391f4990f053f1961
Katia Prigon <katiapr89@gmail.com>
2016-06-21 11:12:14 +0200

    Create GeneralRequestModels.cs

---
+++ Project/Models/GeneralRequestModels.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YotzimLilmod.Models
+{
+    public class GeneralRequestModels
+    {
+        public string Message { get; set; }
+        public int code { get; set; }
+    }
+}
